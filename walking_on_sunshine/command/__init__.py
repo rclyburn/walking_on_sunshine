d3b7189cf63790dc2e994cb52ebb0d9bf5f08f96
Reid Clyburn <reidclyburn@gmail.com>
2025-07-17 13:45:03 -0700

    fixed init

--- walking_on_sunshine/command/__init__.py
+++ walking_on_sunshine/command/__init__.py
@@ -2,4 +2,3 @@
 
 from walking_on_sunshine.command.hello_cmd import hello
 from walking_on_sunshine.command.get_album_length_cmd import get_album_length
-from walking_on_sunshine.command.serve_cmd import serve
