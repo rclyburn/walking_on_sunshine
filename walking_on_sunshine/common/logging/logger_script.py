e138308411d41904da7e6383bef82b746d839a6e
Reid Clyburn <reidclyburn@gmail.com>
2025-07-23 19:18:20 -0700

    working round trip

---
+++ walking_on_sunshine/common/logging/logger_script.py
@@ -0,0 +1,45 @@
+import sys
+
+import structlog
+from structlog.stdlib import BoundLogger
+
+
+def get_logger(name: str) -> BoundLogger:
+    return structlog.get_logger(name)
+
+
+def init_logger():
+    """Initialize the logger"""
+    shared_processors = [
+        structlog.contextvars.merge_contextvars,
+        structlog.processors.add_log_level,
+        structlog.dev.set_exc_info,
+        structlog.processors.CallsiteParameterAdder(
+            parameters={
+                structlog.processors.CallsiteParameter.FILENAME,
+                structlog.processors.CallsiteParameter.LINENO,
+                structlog.processors.CallsiteParameter.FUNC_NAME,
+            },
+        ),
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.ExceptionRenderer(),
+        structlog.processors.UnicodeDecoder(),
+        structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S"),
+    ]
+
+    processors = []
+    if sys.stderr.isatty():
+        # Pretty printing when we run in a terminal session.
+        # Automatically prints pretty tracebacks when "rich" is installed
+        processors = [*shared_processors, structlog.dev.ConsoleRenderer()]
+    else:
+        # Print JSON when we run, e.g., in a Docker container.
+        # Also print structured tracebacks.
+        processors = [
+            *shared_processors,
+            structlog.processors.dict_tracebacks,
+            structlog.processors.JSONRenderer(),
+        ]
+
+    structlog.configure(processors=processors)
---
+++ walking_on_sunshine/scripts/openroute_script.py
@@ -0,0 +1,74 @@
+import os
+import pprint
+import subprocess
+
+import folium
+import openrouteservice
+
+key = os.getenv("OPENROUTE_API_KEY")
+key = str(key)[1:]  # for some reason the key string was returning with an extra ' at the front
+
+
+client = openrouteservice.Client(key=key)  # Specify your personal API key
+
+address = "4050 17th St, San Francisco"
+
+m = folium.Map(location=[37.76256, -122.436414], tiles="cartodbpositron", zoom_start=13)
+
+geocode1 = client.pelias_search(
+    text=address,
+    focus_point=list(reversed(m.location)),
+    validate=False,
+)
+
+geocode2 = client.pelias_search(
+    text="3600 16th St, San Francisco, CA 94114",
+    focus_point=list(reversed(m.location)),
+    validate=False,
+)
+
+# pprint.pprint(geocode)
+
+for result in geocode1["features"]:
+    folium.Marker(
+        location=list(reversed(result["geometry"]["coordinates"])),
+        icon=folium.Icon(icon="compass", color="green", prefix="fa"),
+        popup=folium.Popup(result["properties"]["name"]),
+    ).add_to(m)
+
+
+coordinates = [
+    list(geocode1["features"][0]["geometry"]["coordinates"]),
+    # list(geocode1["features"][0]["geometry"]["coordinates"]),
+]
+
+
+route = client.directions(
+    coordinates=coordinates,
+    profile="foot-walking",
+    format="geojson",
+    instructions=False,
+    validate=False,
+    options={
+        "avoid_features": ["fords", "ferries"],
+        "profile_params": {"weightings": {"green": 0, "quiet": 0}},
+        "round_trip": {
+            "length": 2100,
+            "points": 1000,
+        },
+    },
+)
+
+pprint.pprint(route)
+
+folium.PolyLine(locations=[list(reversed(coord)) for coord in route["features"][0]["geometry"]["coordinates"]]).add_to(
+    m
+)
+
+
+m.save("index.html")
+
+wsl_path = os.path.abspath("index.html")
+windows_path = subprocess.check_output(["wslpath", "-w", wsl_path]).decode().strip()
+
+subprocess.run(["explorer.exe", windows_path])  # opens the map file in the users default browser
