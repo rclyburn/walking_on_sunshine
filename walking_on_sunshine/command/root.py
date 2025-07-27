def2913665bbf8f398a7ece649a8180cc8de581d
rclyburn <166553328+rclyburn@users.noreply.github.com>
2025-07-27 14:03:49 -0700

    logging (#6)
    Co-authored-by: dskart <dskart11@gmail.com>

--- walking_on_sunshine/command/root.py
+++ walking_on_sunshine/command/root.py
@@ -1,7 +1,20 @@
+import logging
+
 import click
+import structlog
 from dotenv import load_dotenv
 
 
 @click.group()
-def root_cmd():
+@click.option("-v", "--verbose", is_flag=True)
+def root_cmd(verbose: bool):
+    if verbose:
+        structlog.configure(
+            wrapper_class=structlog.make_filtering_bound_logger(logging.DEBUG),
+        )
+    else:
+        structlog.configure(
+            wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+        )
+
     load_dotenv()
