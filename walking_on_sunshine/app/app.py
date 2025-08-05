1e1ff224b3c4e72fa8afe0da55b9768cd2a35f9b
Reid Clyburn <reidclyburn@gmail.com>
2025-08-04 17:33:52 -0700

    fixed merge issues

--- walking_on_sunshine/app/app.py
+++ walking_on_sunshine/app/app.py
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 from walking_on_sunshine.app.config import Config
 
 
 class App:
     def __init__(self, config: Config):
         self.config = config
+=======
+class App:
+    def __init__(self):
+        pass
+>>>>>>> 73f8311 (class stuff and early configs)
