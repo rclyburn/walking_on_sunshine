4c31e94010eb682407ed5539ae1ec3ea4f5a8c07
rclyburn <166553328+rclyburn@users.noreply.github.com>
2025-08-06 16:25:24 -0700

    Start command (#8)

--- walking_on_sunshine/app/path_gen.py
+++ walking_on_sunshine/app/path_gen.py
@@ -1,3 +1,46 @@
+import openrouteservice
+
+
 class PathGen:
-    def __init__(self):
+    def __init__(self, key: str | None):
+        self.client = openrouteservice.Client(key=key)
+
+    def get_coords(self, location: str | list, distance: int) -> list:
+        if isinstance(location, str):
+            coordinates = self._addr_to_coords(location)
+        else:
+            coordinates = location
+
+        route = self.client.directions(
+            coordinates=coordinates,
+            profile="foot-walking",
+            format="geojson",
+            instructions=True,
+            validate=False,
+            options={
+                "avoid_features": ["fords", "ferries"],
+                "profile_params": {"weightings": {"green": 1, "quiet": 0}},
+                "round_trip": {"length": distance, "points": 20},
+            },
+        )
+
+        coord_list = list(route["features"][0]["geometry"]["coordinates"])
+        return coord_list
+
+    def _addr_to_coords(self, addr: str):
+        geocode = self.client.pelias_search(
+            text=addr,
+            validate=False,
+        )
+
+        coordinates = [
+            list(geocode["features"][0]["geometry"]["coordinates"]),
+        ]
+
+        return coordinates
+
+    def get_maps_url(self, downsampled_coords: list):
+        pass
+
+    def downsample_coords(self, route_coords):
         pass
