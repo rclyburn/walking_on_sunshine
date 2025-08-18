08e7199702436ac483b0a885c15713449e513f4d
Reid Clyburn <reidclyburn@gmail.com>
2025-08-18 10:22:40 -0700

    api updates

--- walking_on_sunshine/api/api.py
+++ walking_on_sunshine/api/api.py
@@ -1,12 +1,31 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 fast_api = FastAPI()
+templates = Jinja2Templates(directory="walking_on_sunshine/api/templates")
 
 
-@fast_api.get("/")
-def read_root():
-    return {"Hello": "World"}
+@fast_api.get("/{name}/hello_world", response_class=HTMLResponse)
+def read_root(name: str):
+    return f"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>{name}</h1>
+</body>
+</html>
+"""
+
+
+@fast_api.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse(request=request, name="item.html", context={"id": id})
 
 
 @fast_api.post("/")
--- walking_on_sunshine/command/__init__.py
+++ walking_on_sunshine/command/__init__.py
@@ -3,3 +3,4 @@
 from walking_on_sunshine.command.hello_cmd import hello
 from walking_on_sunshine.command.get_album_length_cmd import get_album_length
 from walking_on_sunshine.command.start_cmd import start
+from walking_on_sunshine.command.serve_cmd import serve
