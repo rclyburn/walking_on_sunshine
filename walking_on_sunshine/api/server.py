2b4b33266a05af3feb471258c449aa1d92634d7c
Reid Clyburn <reidclyburn@gmail.com>
2025-12-09 11:20:08 -0800

    railway command pt2

--- walking_on_sunshine/api/server.py
+++ walking_on_sunshine/api/server.py
@@ -1,9 +1,10 @@
 # walking_on_sunshine/api/server.py
 from walking_on_sunshine.api.api import API
 from walking_on_sunshine.app.app import App
+from walking_on_sunshine.app.config import Config as AppConfig
 from walking_on_sunshine.command.config import RootConfig
 
 root_cfg = RootConfig()
-assert root_cfg.app is not None, "app config is required"
-api = API(App(root_cfg.app))
-app = api.fast_api  # uvicorn will import this
+app_cfg = root_cfg.app or AppConfig()  # defaults every field to None
+api = API(App(app_cfg))
+app = api.fast_api
